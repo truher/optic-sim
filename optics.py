d76fe1b2ccac406a6ec08a959883d687edaa60db
Joel Truher <joel@truher.org>
2022-07-25 12:25:03 -0700

    moved simulator out of colab to do parameter study

---
+++ optics.py
@@ -0,0 +1,358 @@
+# pylint: disable=invalid-name, missing-function-docstring, missing-class-docstring, protected-access, too-many-branches, too-few-public-methods
+""" A simple photon simulator.
+
+This is cribbed from https://github.com/DCC-Lab/PyTissueOptics.
+"""
+
+from __future__ import annotations
+import copy
+import math
+import random
+from typing import List, Tuple
+import numpy as np
+
+class Vector:
+    """ Supports rotation."""
+    def __init__(self, x: float = 0, y: float = 0, z: float = 0):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    def any_perpendicular(self) -> Vector:
+        """ make er for ez"""
+        if self._z < self._x:
+            return Vector(self._y, -self._x, 0)
+        return Vector(0, -self._z, self._y)
+
+    def normalize(self) -> Vector:
+        ux = self._x
+        uy = self._y
+        uz = self._z
+
+        norm = ux * ux + uy * uy + uz * uz
+        if norm != 0:
+            invLength = norm ** (-0.5)
+            self._x *= invLength
+            self._y *= invLength
+            self._z *= invLength
+        else:
+            raise ValueError("You cannot normalize the null vector")
+
+        return self
+
+    def rotateAround(self, u: Vector, theta: float) -> Vector:
+        u.normalize()
+
+        cost = np.cos(theta)
+        sint = np.sin(theta)
+        one_cost = 1 - cost
+
+        ux = u._x
+        uy = u._y
+        uz = u._z
+
+        X = self._x
+        Y = self._y
+        Z = self._z
+
+        self._x = (
+            (cost + ux * ux * one_cost) * X
+            + (ux * uy * one_cost - uz * sint) * Y
+            + (ux * uz * one_cost + uy * sint) * Z
+        )
+        self._y = (
+            (uy * ux * one_cost + uz * sint) * X
+            + (cost + uy * uy * one_cost) * Y
+            + (uy * uz * one_cost - ux * sint) * Z
+        )
+        self._z = (
+            (uz * ux * one_cost - uy * sint) * X
+            + (uz * uy * one_cost + ux * sint) * Y
+            + (cost + uz * uz * one_cost) * Z
+        )
+        return self
+
+    def __str__(self) -> str:
+        return f"Vector: x={self._x:.4f} y={self._y:.4f} z={self._z:.4f})"
+
+
+class UnitVector(Vector):
+    def __init__(self, x: float, y: float, z: float):
+        Vector.__init__(self, x, y, z)
+
+    @staticmethod
+    def spherical(theta: float, phi: float) -> UnitVector:
+        x = np.sin(theta) * np.cos(phi)
+        y = np.sin(theta) * np.sin(phi)
+        z = np.cos(theta)
+        return UnitVector(x,y,z)
+
+    @staticmethod
+    def cartesian(x: float, y: float, z: float) -> UnitVector:
+        v = Vector(x,y,z).normalize()
+        return UnitVector(v._x, v._y, v._z)
+
+    def phi(self) -> float:
+        """ Returns [-pi, pi]."""
+        # use np.arctan2 in vectorized version
+        return math.atan2(self._y, self._x)
+
+    def theta(self) -> float:
+        # use np.arccos in vectorized version
+        return math.acos(self._z)
+
+class Photon:
+    def __init__(self, R: Vector, EZ: UnitVector):
+        self.r = R # local coordinate position
+        self.ez = EZ # propagation direction
+        self.er = self.ez.any_perpendicular() # perpendicular to direction, used for rotation
+        self.weight = 1 # zero means ignore.
+
+    @staticmethod
+    def spherical(x: float, y: float, z: float, theta: float, phi: float) -> Photon:
+        # local coordinate position
+        r = Vector(x, y, z)
+        # propagation direction
+        ez = UnitVector.spherical(theta, phi)
+        return Photon(r, ez)
+
+    def scatterBy(self, theta: float, phi: float) -> None:
+        """Mutates this photon."""
+        self.er.rotateAround(self.ez, phi)
+        self.ez.rotateAround(self.er, theta)
+
+    @property
+    def isAlive(self) -> bool:
+        return self.weight > 0
+
+    @staticmethod
+    def sample(photons: List[Photon], N: int) -> List[Photon]:
+        return copy.deepcopy(random.sample(photons, min(N, len(photons))))
+
+    @staticmethod
+    def countAlive(photons: List[Photon]) -> int:
+        count = 0
+        for p in photons:
+            if p.isAlive:
+                count += 1
+        return count
+
+    @staticmethod
+    def selectAlive(photons: List[Photon]) -> List[Photon]:
+        alive = []
+        for p in photons:
+            if p.isAlive:
+                alive.append(p)
+        return alive
+
+    def __str__(self) -> str:
+        return (f"Photon: x={self.r._x:.2f} y={self.r._y:.2f} z={self.r._z:.2f} "
+                f"theta={self.ez.theta():.2f} phi={self.ez.phi():.2f}")
+
+
+class Source:
+    def newPhoton(self) -> Photon:
+        raise NotImplementedError()
+
+    def make_photons(self, photon_count: int) -> List[Photon]:
+        photons = []
+        for pi in range(photon_count): # pylint: disable=unused-variable
+            p = self.newPhoton()
+            photons.append(p)
+        return photons
+
+
+class PencilSource(Source):
+    """Zero area zero divergence."""
+
+    def newPhoton(self) -> Photon:
+        return Photon.spherical(0, 0, 0, 0, 0)
+
+
+class HalfSphereSource(Source):
+    """Isotropic intensity in the upper half-sphere."""
+
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+
+    def newPhoton(self) -> Photon:
+        x = self._width * (np.random.random() - 0.5)
+        y = self._height * (np.random.random() - 0.5)
+        phi = np.random.random() * 2 * np.pi
+        theta = np.arccos(np.random.random())
+        p = Photon.spherical(x, y, 0, 0, 0)
+        p.scatterBy(theta, phi)
+        return p
+
+    def __str__(self) -> str:
+        return f"HalfSphereSource: width={self._width:.2f} height={self._height:.2f}"
+
+
+class LambertianSource(Source):
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+
+    def newPhoton(self) -> Photon:
+        x = self._width * (np.random.random() - 0.5)
+        y = self._height * (np.random.random() - 0.5)
+        phi = np.random.random() * 2 * np.pi
+        theta = np.arccos(2 * np.random.random() - 1) / 2
+        p: Photon = Photon.spherical(x, y, 0, theta, phi)
+        return p
+
+
+class RealLEDSource(Source):
+    """TODO: use the published data from Cree."""
+
+    def __init__(self, width: float, height: float):
+        self._width = width  # microns
+        self._height = height  # microns
+
+    def newPhoton(self) -> Photon:
+        x = self._width * (np.random.random() - 0.5)
+        y = self._height * (np.random.random() - 0.5)
+        phi = np.random.random() * 2 * np.pi
+        theta = np.arccos(2 * np.random.random() - 1) / 2
+        p: Photon = Photon.spherical(x, y, 0, theta, phi)
+        return p
+
+
+class Diffuser:
+    """ Something that changes photon direction.
+
+    Now mutates rather than copying.
+    Examples: diffuser, retroreflector.
+    """
+    def __init__(self, g: float, absorption: float):
+        """
+            g: Henyey and Greenstein scattering parameter.
+                0 is iso, 1 is no scattering, -1 is reflection.
+            absorption: mostly useful for the diffuser
+        """
+        self._g = g
+        self._absorption = absorption
+
+    def getScatteringAngles(self) -> Tuple[float, float]:
+        """ Henyey and Greenstein scattering. This is from material.py. """
+        phi = np.random.random() * 2 * np.pi
+        temp = (1 - self._g * self._g) / (1 - self._g + 2 * self._g * np.random.random())
+        cost = (1 + self._g * self._g - temp * temp) / (2 * self._g)
+        return np.arccos(cost), phi
+
+    def diffuse(self, photons: List[Photon]) -> None:
+        """Adjust propagation direction."""
+        for p in photons:
+            if not p.isAlive:
+                continue
+            if np.random.random() < self._absorption:
+                p.weight = 0
+                continue
+            # use the actual diffuser angular distribution here?
+            theta_scatter, phi_scatter = self.getScatteringAngles()
+            p.scatterBy(theta_scatter, phi_scatter)
+
+
+class Lightbox:
+    """ Represents the box between the source and diffuser.
+
+    Sides are somewhat reflective.
+    Now mutates rather than copying.
+    """
+    def __init__(self, height: float, size: float):
+        """
+        height: top of the box above the source
+        size: full length or width, box is square.
+        """
+        self._height = height
+        self._size = size
+
+    def propagate(self, photons: List[Photon]) -> None:
+        """ Propagate (mutate) photons through the light box to the top."""
+
+        absorption = 0.1 # polished metal inside
+        for p in photons:
+            # photon starts at p.r(x,y).  assume p.r(z) is zero (todo: fix that)
+            if not p.isAlive:
+                continue
+            if p.ez._z < 0:
+                p.weight = 0
+                continue # this shouldn't happen
+
+            p.r._x = p.r._x + self._height * p.ez._x / p.ez._z
+            p.r._y = p.r._y + self._height * p.ez._y / p.ez._z
+            p.r._z = self._height
+
+            done_reflecting = False
+            while not done_reflecting:
+                if p.r._x < -self._size/2:
+                    p.r._x = -self._size - p.r._x
+                    p.ez._x *= -1
+                    p.er._x *= -1
+                    if np.random.random() < absorption:
+                        break
+                elif p.r._x > self._size/2:
+                    p.r._x = self._size - p.r._x
+                    p.ez._x *= -1
+                    p.er._x *= -1
+                    if np.random.random() < absorption:
+                        break
+                elif p.r._y < -self._size/2:
+                    p.r._y = -self._size - p.r._y
+                    p.ez._y *= -1
+                    p.er._y *= -1
+                    if np.random.random() < absorption:
+                        break
+                elif p.r._y > self._size/2:
+                    p.r._y = self._size - p.r._y
+                    p.ez._y *= -1
+                    p.er._y *= -1
+                    if np.random.random() < absorption:
+                        break
+                if (p.r._x >= -self._size / 2 and p.r._x <= self._size / 2
+                    and p.r._y >= -self._size / 2 and p.r._y <= self._size / 2):
+                    done_reflecting = True
+            if not done_reflecting: # i.e. early break
+                p.weight = 0
+
+
+
+
+def propagateToReflector(photons: List[Photon], location: float, size: float) -> None:
+    """ just push them up there.
+      location: z dimension of the reflector.  it's *far*, if the unit is 100 microns,
+      and the reflector is 1-10 meters away, that's 10000-100000.
+      size: say 10cm on a side, 1000 units.
+    there's no box.
+    todo: make this a class, combine it somehow with the other one above
+    """
+    for p in photons:
+        if not p.isAlive:
+            continue
+        if p.ez._z < 0:
+            p.weight = 0
+            continue
+        distance_z = location - p.r._z
+        p.r._x = p.r._x + distance_z * p.ez._x / p.ez._z
+        p.r._y = p.r._y + distance_z * p.ez._y / p.ez._z
+        p.r._z = location
+        if p.r._x < -size / 2 or p.r._x > size / 2 or p.r._y < -size / 2 or p.r._y > size / 2:
+            p.weight = 0
+
+
+def propagateToCamera(photons: List[Photon], location: float) -> None:
+    """ back down to the camera.
+
+    for now it's just the camera plane. TODO: add a counter.
+    """
+    for p in photons:
+        if not p.isAlive:
+            continue
+        if p.ez._z > 0:
+            p.weight = 0
+            continue
+        distance_z = location - p.r._z # negative number in this case
+        p.r._x = p.r._x + distance_z * p.ez._x / p.ez._z
+        p.r._y = p.r._y + distance_z * p.ez._y / p.ez._z
+        p.r._z = location
---
+++ viz.py
@@ -0,0 +1,233 @@
+# pylint: disable=too-many-statements, too-many-arguments, too-many-locals, consider-using-from-import, protected-access
+""" Plotting functions"""
+
+from __future__ import annotations
+from typing import List
+import matplotlib.pyplot as plt # type: ignore
+import mpl_toolkits.mplot3d.art3d as art3d # type: ignore
+import numpy as np
+from matplotlib.patches import Rectangle # type: ignore
+from mpl_toolkits.mplot3d import Axes3D # pylint: disable=unused-import
+from numpy.typing import NDArray
+import optics
+
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+
+def plot_rays(batches: List[List[optics.Photon]], elev: float, azim: float,
+              size: float, arrow_length: float, rectangles: List[List[float]]) -> None:
+    """3d plot a set of photon batches, each batch a different color.
+
+    Also render interesting rectangles.  [xmin,xmax,ymin,ymax,z]
+    """
+    fig = plt.figure(figsize=[15, 15])
+    axes = fig.gca(projection="3d")
+    for i, p_batch in enumerate(batches):
+        output_len = int(len(p_batch))
+        photon_x = np.zeros([output_len])
+        photon_y = np.zeros([output_len])
+        photon_z = np.zeros([output_len])
+        photon_u = np.zeros([output_len])
+        photon_v = np.zeros([output_len])
+        photon_w = np.zeros([output_len])
+        for pidx, photon in enumerate(p_batch):
+            if not photon.isAlive:
+                continue
+            photon_x[pidx] = photon.r._x
+            photon_y[pidx] = photon.r._y
+            photon_z[pidx] = photon.r._z
+            photon_u[pidx] = arrow_length * photon.ez._x
+            photon_v[pidx] = arrow_length * photon.ez._y
+            photon_w[pidx] = arrow_length * photon.ez._z
+        axes.quiver(
+            photon_x,
+            photon_y,
+            photon_z,
+            photon_u,
+            photon_v,
+            photon_w,
+            arrow_length_ratio=0.05,
+            color=f"C{i%10}",
+        )
+    axes.set_xlim([-size / 2, size / 2])
+    axes.set_ylim([-size / 2, size / 2])
+    axes.set_zlim([0, size])
+    axes.view_init(elev, azim)
+    axes.set_xlabel("X axis")
+    axes.set_ylabel("Y axis")
+    axes.set_zlabel("Z axis")
+    for ridx, rectangle in enumerate(rectangles):
+        xmin = rectangle[0]
+        xmax = rectangle[1]
+        ymin = rectangle[2]
+        ymax = rectangle[3]
+        location_z = rectangle[4]
+        patch = Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=False, linewidth=5,
+                      color=f'C{ridx%10}')
+        axes.add_patch(patch)
+        art3d.pathpatch_2d_to_3d(patch, z=location_z, zdir="z")
+    plt.show()
+
+
+def plot_histogram_slices(photon_batch: List[optics.Photon], suptitle: str,
+                          x_min: float, x_max: float, y_min: float, y_max: float,
+                          z_min: float, z_max: float,
+                          theta_min: float = 0, theta_max: float = np.pi,
+                          phi_min: float = -np.pi, phi_max: float = np.pi,
+                          bins: int = 100) -> None:
+    """Slice counts by x, y, phi, and theta, also show intensity per steradian."""
+    # radiance is *projected* area, so compute that too?
+    # radiosity (W/m^2): all directions, per area (slice in x, total in y)
+    x_freq: NDArray[np.int32] = np.zeros(bins, dtype=np.int32)
+
+    # radiosity (W/m^2): all directions, per area (slice in y, total in x)
+    y_freq: NDArray[np.int32] = np.zeros(bins, dtype=np.int32)
+
+    # this is to find errant photons
+    z_freq: NDArray[np.int32] = np.zeros(bins, dtype=np.int32)
+
+    # radiant intensity (W/sr): whole area, per angle (slice in theta, total in phi)
+    theta_freq: NDArray[np.int32] = np.zeros(bins, dtype=np.int32)
+
+    # radiant intensity (W/sr): whole area, per angle (slice in phi, total in theta)
+    phi_freq: NDArray[np.int32] = np.zeros(bins, dtype=np.int32)
+
+    photons_per_steridian_by_theta: NDArray[np.float64] = np.zeros(bins, dtype=np.float64)
+
+    x_range = x_max - x_min
+    x_bin_width = x_range / bins
+    x_bins = x_range * np.arange(bins) / bins + x_min # units?
+
+    y_range = y_max - y_min
+    y_bin_width = y_range / bins
+    y_bins = y_range * np.arange(bins) / bins + y_min # units?
+
+    z_range = z_max - z_min
+    z_bin_width = z_range / bins
+    z_bins = z_range * np.arange(bins) / bins + z_min
+
+    theta_range = theta_max - theta_min
+    theta_bin_width_rad = theta_range / bins
+    theta_bin_width_deg = theta_bin_width_rad * 180 / np.pi
+
+    theta_bins_rad = (theta_range * np.arange(bins) / bins + theta_min)
+    theta_bins_deg = theta_bins_rad * 180 / np.pi
+
+    phi_range = phi_max - phi_min
+    phi_bin_width_rad = phi_range / bins
+    phi_bin_width_deg = phi_bin_width_rad * 180 / np.pi
+    phi_bins_deg = (phi_range * np.arange(bins) / bins + phi_min) * 180 / np.pi
+
+    for photon in photon_batch:
+        if not photon.isAlive:
+            continue
+        # "floor" so that it doesn't double-count near zero.
+        # figure out the area thing here?
+        x_bin = int((photon.r._x - x_min) // x_bin_width)
+        if x_bin < 0 or x_bin > bins - 1:
+            continue
+        x_freq[x_bin] += 1
+
+        y_bin = int((photon.r._y - y_min) // y_bin_width)
+        if y_bin < 0 or y_bin > bins - 1:
+            continue
+        y_freq[y_bin] += 1
+
+        z_bin = int((photon.r._z - z_min) // z_bin_width)
+        if z_bin < 0 or z_bin > bins - 1:
+            continue
+        z_freq[z_bin] += 1
+
+        theta_bin = int((photon.ez.theta() - theta_min) // theta_bin_width_rad)
+        if theta_bin < 0 or theta_bin > bins - 1:
+            continue
+        theta_freq[theta_bin] += 1
+
+        phi_bin = int((photon.ez.phi() - phi_min) // phi_bin_width_rad)
+        if phi_bin < 0 or phi_bin > bins - 1:
+            continue
+        phi_freq[phi_bin] += 1
+
+        # figure out the area thing here?
+        photons_per_steridian_by_theta[theta_bin] += 1 / np.sin(photon.ez.theta())
+
+    # the above histograms are counts per bucket.
+    # i want counts per steradian.
+    # it's axially symmetric so ignore phi
+    # the size of a theta bucket is
+    #  h = np.cos(left) - np.cos(right)
+    #  area = 2 * np.pi * h
+    # the bin width is np.pi / 100
+
+    theta_bucket_angle_steradians: NDArray[np.float64] = np.array(
+        list(
+            map(
+                lambda x: (np.cos((theta_range * x + theta_min) / bins)
+                - np.cos((theta_range * (x + 1) + theta_min) / bins)),
+                range(bins),
+            )
+        )
+    )
+    photons_per_steradian_by_theta = theta_freq / theta_bucket_angle_steradians
+
+    fig = plt.figure(figsize=(18, 10))
+    fig.suptitle(suptitle)
+
+    axes = plt.subplot(331)
+    plt.bar(x=x_bins, height=x_freq, width=x_bin_width)
+    axes.set_title("photons per bucket by x")
+    axes.set_xlabel("x dimension (TODO: unit)")
+    axes.set_ylabel("photon count per bucket (TODO: density)")
+
+    axes = plt.subplot(332)
+    plt.bar(x=y_bins, height=y_freq, width=y_bin_width)
+    axes.set_title("photons per bucket by y")
+    axes.set_xlabel("y dimension (TODO: unit)")
+    axes.set_ylabel("photon count per bucket (TODO: density)")
+
+    axes = plt.subplot(333)
+    plt.bar(x=z_bins, height=z_freq, width=z_bin_width)
+    axes.set_title("photons per bucket by z")
+    axes.set_xlabel("z dimension (TODO: unit)")
+    axes.set_ylabel("photon count per bucket (TODO: density)")
+
+    axes = plt.subplot(334)
+    plt.bar(x=phi_bins_deg, height=phi_freq, width=phi_bin_width_deg)
+    axes.set_title("photons per bucket by phi")
+    axes.set_xlabel("azimuth (phi) (degrees)")
+    axes.set_ylabel("photon count per bucket (TODO: density)")
+
+    axes = plt.subplot(335)
+    plt.bar(x=theta_bins_deg, height=theta_freq, width=theta_bin_width_deg)
+    axes.set_title("photons per bucket by theta")
+    axes.set_xlabel("polar angle (theta) (degrees)")
+    axes.set_ylabel("photon count per bucket (TODO: density)")
+
+    axes = plt.subplot(336)
+    plt.bar(x=theta_bins_deg, height=photons_per_steradian_by_theta, width=theta_bin_width_deg)
+    axes.set_title("radiant intensity photons/sr by theta by bucket area")
+    axes.set_xlabel("polar angle (theta) (degrees)")
+    axes.set_ylabel("photon count per ... ? (TODO: sr)")
+    # notice the higher variance at the poles; N is lower there, which is not good.
+
+    axes = plt.subplot(337)
+    plt.bar(x=theta_bins_deg, height=photons_per_steridian_by_theta, width=theta_bin_width_deg)
+    axes.set_title("radiant intensity photons/sr by photon weights")
+    axes.set_xlabel("polar angle (theta) (degrees)")
+    axes.set_ylabel("photon count per ... ? (TODO: sr)")
+
+    plt.show()
+
+    fig = plt.figure(figsize=[15, 12])
+    axes = plt.subplot(projection="polar")
+    axes.set_theta_zero_location("N")
+    # mirror the data so it looks nice .. resulting in the weird bounds here
+    axes.set_thetamin(-theta_max * 180 / np.pi)
+    axes.set_thetamax(theta_max * 180 / np.pi)
+    axes.bar(
+        theta_bins_rad, photons_per_steridian_by_theta, width=theta_bin_width_rad, color="black"
+    )
+    axes.bar(
+        -theta_bins_rad, photons_per_steridian_by_theta, width=theta_bin_width_rad, color="black"
+    )
+    axes.set_xlabel("polar angle (theta) (degrees)")
+    axes.set_ylabel("photon count per ... ? (TODO: sr)")
