bacbc9adbbdad5fb425910a61d77f40667d4d201
Joel Truher <joel@truher.org>
2022-07-31 20:13:41 -0700

    move working code out of the notebook

---
+++ stats_cuda.py
@@ -0,0 +1,56 @@
+import cupy as cp  # type:ignore
+import numpy as np
+from cupyx import jit  # type:ignore
+
+
+@jit.rawkernel()
+def histogram(
+    input_data_1: cp.ndarray,
+    input_data_2: cp.ndarray,
+    mapper: np.int32,
+    global_histogram: cp.ndarray,
+    size: np.int32,
+    min_value: np.float32,
+    bin_width: np.float32,
+) -> None:
+    """Histogram with a map function.
+
+    That way you can transform coordinates on the fly, you don't need to store the transformed
+    version.  For example, store cartesian, but histogram spherical.
+
+    input_data_1: cp.ndarray[np.float32]
+    input_data_2: cp.ndarray[np.float32], use for phi, 1:x, 2:y
+    mapper: 0 = none, 1 = phi, 2 = theta
+    global_histogram: cp.ndarray[np.int32]
+    """
+    # Create a histogram for this block.  Size must match bins and threads.
+    block_histogram = jit.shared_memory(np.int32, 128)
+
+    # Alloc is not zeroed so do it.
+    block_histogram[jit.threadIdx.x] = 0
+
+    # Wait for all threads to set their zeros.
+    jit.syncthreads()
+
+    # Fill up the block-local histogram.
+    # Adjacent threads get adjacent data:
+    tid = jit.blockIdx.x * jit.blockDim.x + jit.threadIdx.x
+    # If there's more than one grid (not recommended) then do that too:
+    ntid = jit.gridDim.x * jit.blockDim.x
+
+    for i in range(tid, size, ntid):
+        if mapper == 1:  # phi
+            mapped_data = cp.arctan2(input_data_2[i], input_data_1[i])
+        elif mapper == 2:  # theta
+            mapped_data = cp.arccos(input_data_1[i])
+        else:
+            mapped_data = input_data_1[i]
+        bucket_idx = int((mapped_data - min_value) // bin_width)
+        bucket_idx = int(min(max(bucket_idx, 0), 127))  # must match above
+        jit.atomic_add(block_histogram, bucket_idx, 1)
+
+    # Wait for all the threads to do it.
+    jit.syncthreads()
+
+    # Sum the block-local histograms into a global histogram.
+    jit.atomic_add(global_histogram, jit.threadIdx.x, block_histogram[jit.threadIdx.x])
--- viz_cuda.py
+++ viz_cuda.py
@@ -1,7 +1,17 @@
-import optics_cuda 
+import math
+import matplotlib # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+import numpy as np
+import warnings
+import optics_cuda
+from stats_cuda import *
+
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
+
 
 def plot_histogram_slices(
-    photon_batch: optics.Photons,
+    photon_batch: optics_cuda.Photons,
+    size: np.int32,
     suptitle: str,
     x_min: float,
     x_max: float,
@@ -13,64 +23,141 @@ def plot_histogram_slices(
     theta_max: float = np.pi,
     phi_min: float = -np.pi,
     phi_max: float = np.pi,
-    bins: int = 100) -> None:
+) -> None:
 
+    bins = 128  # matches threads etc
+    threads_per_block = bins  # because the threads write back
+    grid_size = (int(math.ceil(size / threads_per_block)), 1, 1)
+    block_size = (threads_per_block, 1, 1)
 
+    null_vector = cp.empty(0, dtype=np.float32)
     fig = plt.figure(figsize=(18, 10))
     fig.suptitle(suptitle)
 
+    h = cp.zeros(bins, dtype=np.int32)
+    histogram(
+        grid_size,
+        block_size,
+        (
+            photon_batch.r_x,
+            null_vector,
+            np.int32(0),
+            h,
+            size,
+            np.float32(x_min),
+            np.float32((x_max - x_min) / bins),
+        ),
+    )
     axes = plt.subplot(331)
-    plt.hist(photon_batch.r_x.get(), bins = bins)
+    plt.plot(np.linspace(x_min, x_max, bins), h.get(), snap=False)
     axes.set_title("photons per bucket by x")
     axes.set_xlabel("x dimension (TODO: unit)")
     axes.set_ylabel("photon count per bucket (TODO: density)")
 
+    h = cp.zeros(bins, dtype=np.int32)
+    histogram(
+        grid_size,
+        block_size,
+        (
+            photon_batch.r_y,
+            null_vector,
+            np.int32(0),
+            h,
+            size,
+            np.float32(y_min),
+            np.float32((y_max - y_min) / bins),
+        ),
+    )
     axes = plt.subplot(332)
-    plt.hist(photon_batch.r_y.get(), bins=bins)
+    plt.plot(np.linspace(y_min, y_max, bins), h.get(), snap=False)
     axes.set_title("photons per bucket by y")
     axes.set_xlabel("y dimension (TODO: unit)")
     axes.set_ylabel("photon count per bucket (TODO: density)")
 
+    h = cp.zeros(bins, dtype=np.int32)
+    histogram(
+        grid_size,
+        block_size,
+        (
+            photon_batch.r_z,
+            null_vector,
+            np.int32(0),
+            h,
+            size,
+            np.float32(z_min),
+            np.float32((z_max - z_min) / bins),
+        ),
+    )
     axes = plt.subplot(333)
-    plt.hist(photon_batch.r_z, bins=bins)
+    plt.plot(np.linspace(z_min, z_max, bins), h.get(), snap=False)
     axes.set_title("photons per bucket by z")
     axes.set_xlabel("z dimension (TODO: unit)")
     axes.set_ylabel("photon count per bucket (TODO: density)")
 
+    h = cp.zeros(bins, dtype=np.int32)
+    histogram(
+        grid_size,
+        block_size,
+        (
+            photon_batch.ez_y,
+            photon_batch.ez_x,
+            np.int32(1),
+            h,
+            size,
+            np.float32(phi_min),
+            np.float32((phi_max - phi_min) / bins),
+        ),
+    )
     axes = plt.subplot(334)
-    plt.hist(x=phi_bins_deg, height=phi_freq, width=phi_bin_width_deg)
+    plt.plot(np.linspace(phi_min, phi_max, bins), h.get(), snap=False)
     axes.set_title("photons per bucket by phi")
     axes.set_xlabel("azimuth (phi) (degrees)")
     axes.set_ylabel("photon count per bucket (TODO: density)")
 
+    h = cp.zeros(bins, dtype=np.int32)
+    histogram(
+        grid_size,
+        block_size,
+        (
+            photon_batch.ez_z,
+            null_vector,
+            np.int32(2),
+            h,
+            size,
+            np.float32(theta_min),
+            np.float32((theta_max - theta_min) / bins),
+        ),
+    )
     axes = plt.subplot(335)
-    plt.bar(x=theta_bins_deg, height=theta_freq, width=theta_bin_width_deg)
+    plt.plot(np.linspace(theta_min, theta_max, bins), h.get(), snap=False)
     axes.set_title("photons per bucket by theta")
     axes.set_xlabel("polar angle (theta) (degrees)")
     axes.set_ylabel("photon count per bucket (TODO: density)")
 
+    theta_range = theta_max - theta_min
+    theta_bins_rad = theta_range * np.arange(bins) / bins + theta_min
+    theta_bin_width_rad = theta_range / bins
+    theta_bucket_angle_steradians = np.array(
+        list(
+            map(
+                lambda x: (
+                    np.cos((theta_range * x + theta_min) / bins)
+                    - np.cos((theta_range * (x + 1) + theta_min) / bins)
+                ),
+                range(bins),
+            )
+        )
+    )
+    photons_per_steradian_by_theta = h.get() / theta_bucket_angle_steradians
     axes = plt.subplot(336)
-    plt.bar(
-        x=theta_bins_deg,
-        height=photons_per_steradian_by_theta,
-        width=theta_bin_width_deg,
+    plt.plot(
+        np.linspace(theta_min, theta_max, bins),
+        photons_per_steradian_by_theta,
+        snap=False,
     )
     axes.set_title("radiant intensity photons/sr by theta by bucket area")
     axes.set_xlabel("polar angle (theta) (degrees)")
     axes.set_ylabel("photon count per ... ? (TODO: sr)")
-    # notice the higher variance at the poles; N is lower there, which is not good.
-
-    axes = plt.subplot(337)
-    plt.bar(
-        x=theta_bins_deg,
-        height=photons_per_steridian_by_theta,
-        width=theta_bin_width_deg,
-    )
-    axes.set_title("radiant intensity photons/sr by photon weights")
-    axes.set_xlabel("polar angle (theta) (degrees)")
-    axes.set_ylabel("photon count per ... ? (TODO: sr)")
-
-    plt.show()
 
     fig = plt.figure(figsize=[15, 12])
     axes = plt.subplot(projection="polar")
@@ -78,17 +165,7 @@ def plot_histogram_slices(
     # mirror the data so it looks nice .. resulting in the weird bounds here
     axes.set_thetamin(-theta_max * 180 / np.pi)
     axes.set_thetamax(theta_max * 180 / np.pi)
-    axes.bar(
-        theta_bins_rad,
-        photons_per_steridian_by_theta,
-        width=theta_bin_width_rad,
-        color="black",
-    )
-    axes.bar(
-        -theta_bins_rad,
-        photons_per_steridian_by_theta,
-        width=theta_bin_width_rad,
-        color="black",
-    )
+    axes.plot(theta_bins_rad, photons_per_steradian_by_theta, color="blue", snap=False)
+    axes.plot(-theta_bins_rad, photons_per_steradian_by_theta, color="blue", snap=False)
     axes.set_xlabel("polar angle (theta) (degrees)")
-    axes.set_ylabel("photon count per ... ? (TODO: sr)")) -> None:
+    axes.set_ylabel("photon count per ... ? (TODO: sr)")
