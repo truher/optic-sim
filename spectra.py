1a938e513e3cc8410b6763e8a7d25ac70fe40780
Joel Truher <joel@truher.org>
2022-08-16 17:57:53 -0700

    fixed photon flux, demo camera etc

--- spectra.py
+++ spectra.py
@@ -3,18 +3,35 @@ import cupy as cp
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.constants
 
 
 class Spectrum(Enum):
-    def __init__(self, name, file_name):
+    def __init__(self, name, file_name, converters=None):
         self._name = name
+        if converters is None:
+            converters={1: lambda x: float(x.strip("%")) / 100}
         df = pd.read_csv(
             f"spectra/{file_name}",
             delimiter="\t",
-            converters={1: lambda x: float(x.strip("%")) / 100},
+            converters=converters
         )
         self._x = cp.arange(360, 780, 0.1)
+        # _y is expressed in energy terms
         self._y = cp.interp(self._x, cp.array(df.iloc[:,0].to_numpy()), cp.array(df.iloc[:,1].to_numpy()))
+
+        # the above data are expressed in terms of power, not quanta (with
+        # the exception of quantum efficiency which is handled below).
+        # so convert the pdf to quanta:
+        nm = self._x
+        spectral_flux_j_s_nm = self._y
+
+        wavelength_m = nm * 1e-9
+        frequency_hz = scipy.constants.c / wavelength_m
+        energy_per_photon_j = scipy.constants.h * frequency_hz
+        # actually per bundle
+        self._spectral_photon_flux_photons_s_nm = spectral_flux_j_s_nm / energy_per_photon_j
+
 
     def pdf(self):
         return (self._x, self._y)
@@ -22,7 +39,11 @@ class Spectrum(Enum):
 
 class Emitter(Spectrum):
     def emit(self, size) -> cp.array:
-        return cp.array(SourceSpectrum.generate_rand_from_pdf(self._y, self._x, size))
+        # photon emission is scaled by photon *energy*
+        #return cp.array(Emitter.generate_rand_from_pdf(self._y, self._x, size))
+        # so use the photon flux here instead.
+        return cp.array(Emitter.generate_rand_from_pdf(self._spectral_photon_flux_photons_s_nm,
+                                                       self._x, size))
 
     @staticmethod
     def generate_rand_from_pdf(pdf: cp.ndarray, x_grid: cp.ndarray, size) -> pd.Series:
@@ -48,12 +69,14 @@ class Emitter(Spectrum):
 
 
 class Absorber(Spectrum):
-    def __init__(self, name, file_name):
-        super().__init__(name, file_name)
+    def __init__(self, name, file_name, converters = None):
+        super().__init__(name, file_name, converters)
         self._rng = cp.random.default_rng()
 
     def absorb(self, photon_wavelength:cp.ndarray, photon_alive:cp.ndarray):
         filter_idx = cp.searchsorted(self._x, photon_wavelength)
+        # even though _y measures energy it actually measures the (spectral) ratio of output to input
+        # which is the same no matter how you measure it.
         filter_value = self._y[filter_idx]
         fate = self._rng.random(photon_alive.size) < filter_value
         cp.logical_and(photon_alive, fate, out=photon_alive)
@@ -67,8 +90,24 @@ class Absorber(Spectrum):
         plt.ylim(0, 1)
         plt.show()
 
+class LuminousEfficiency(Absorber):
+    def __init__(self, name, file_name):
+        super().__init__(name, file_name, converters={})
+        
+        wavelength_m = self._x * 1e-9
+        frequency_hz = scipy.constants.c / wavelength_m
+        energy_per_photon_j = scipy.constants.h * frequency_hz
+        lumen_seconds_per_joule_per_nm = self._y * 683
+        self._lumen_seconds_per_photon_per_nm = lumen_seconds_per_joule_per_nm * energy_per_photon_j
+
+    def lumen_seconds(self, photon_wavelength_nm, photons_per_bundle, photon_alive):
+        # actually per bundle
+        idx = cp.searchsorted(self._x, photon_wavelength_nm)
+        return cp.sum(self._lumen_seconds_per_photon_per_nm[idx]) * photons_per_bundle
+
 
 class SourceSpectrum(Emitter):
+    # these curves describe spectral flux in terms of power produced per wavelength
     LED_AMBER = ("Amber", "Spectra - Cree xp-e2 amber.tsv")
     LED_BLUE = ("Blue", "Spectra - Cree xp-e2 blue.tsv")
     LED_COOL_WHITE = ("Cool White", "Spectra - Cree xp-e2 cool white.tsv")
@@ -83,6 +122,7 @@ class SourceSpectrum(Emitter):
 
 
 class FilterSpectrum(Absorber):
+    # these curves describe spectral flux in terms of power absorbed per wavelength
     FILTER_27 = ("27 Medium Red", "Spectra - Roscolux 27.tsv")
     FILTER_389 = ("389 Chroma Green", "Spectra - Roscolux 389.tsv")
     FILTER_4600 = ("4600 Medium Red Blue", "Spectra - Roscolux 4600.tsv")
@@ -90,10 +130,17 @@ class FilterSpectrum(Absorber):
     FILTER_6500 = ("6500 Primary Red", "Spectra - Roscolux 6500.tsv")
     FILTER_74 = ("74 Night Blue", "Spectra - Roscolux 74.tsv")
 
+class Photopic(LuminousEfficiency):
+    # This is the Sharpe, Stockman, Jagla & Gagle 2005 version, not the 1931 CIE version
+    # so it is slightly more sensitive to blue: wikipedia.org/wiki/Luminous_efficiency_function
+    # it is expressed as power per wavelength.
+    PHOTOPIC = ("Photopic Response", 'Spectra - Photopic.tsv')
 
 class CameraSpectrum(Absorber):
+    # QE is the ratio of photons absorbed to electrons produced
     CAMERA_SEE_3_CAM = ("Quantum Efficiency", "Spectra - QE.tsv")
-    PHOTOPIC = ("Photopic Response", 'Spectra - Photopic.tsv')
+
+
 
 
 def compare_all():
